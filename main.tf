1564ef43fa86545ef89cf8f1724d9ba86e1493bd
dkool <dennis.kool@cloudnation.nl>
2022-05-09 13:29:39 +0200

    add mule nsg rule arguments

--- main.tf
+++ main.tf
@@ -101,15 +101,20 @@ resource "azurerm_network_security_group" "nsg" {
     for_each = each.value.rules
 
     content {
-      name                       = security_rule.value.name
-      priority                   = security_rule.value.priority
-      direction                  = security_rule.value.direction
-      access                     = security_rule.value.access
-      protocol                   = security_rule.value.protocol
-      source_port_range          = security_rule.value.source_port_range
-      destination_port_range     = security_rule.value.destination_port_range
-      source_address_prefix      = security_rule.value.source_address_prefix
-      destination_address_prefix = security_rule.value.destination_address_prefix
+      name                         = security_rule.value.name
+      priority                     = security_rule.value.priority
+      direction                    = security_rule.value.direction
+      access                       = security_rule.value.access
+      protocol                     = security_rule.value.protocol
+      description                  = lookup(security_rule.value, "description", null)
+      source_port_range            = lookup(security_rule.value, "source_port_range", null)
+      source_port_ranges           = lookup(security_rule.value, "source_port_ranges", null)
+      destination_port_range       = lookup(security_rule.value, "destination_port_range", null)
+      destination_port_ranges      = lookup(security_rule.value, "destination_port_ranges", null)
+      source_address_prefix        = lookup(security_rule.value, "source_address_prefix", null)
+      source_address_prefixes      = lookup(security_rule.value, "source_address_prefixes", null)
+      destination_address_prefix   = lookup(security_rule.value, "destination_address_prefix", null)
+      destination_address_prefixes = lookup(security_rule.value, "destination_address_prefixes", null)
     }
   }
 }
