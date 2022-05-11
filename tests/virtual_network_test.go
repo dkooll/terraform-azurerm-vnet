8b83ea4610461fcfc6b63a0e9a4d08082a690a2f
dkool <dennis.kool@cloudnation.nl>
2022-05-11 14:20:58 +0200

    function rename and fix output escape codes

--- tests/virtual_network_test.go
+++ tests/virtual_network_test.go
@@ -4,17 +4,22 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/require"
 )
 
-func TestTerraformAwsHelloWorldExample(t *testing.T) {
+func TestApplyNoError(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 
 		TerraformDir: "../",
 		Parallelism:  2,
+		//NoColor:      true,
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
+	out := terraform.InitAndApply(t, terraformOptions)
+
+	// Check that NoColor correctly doesn't output the colour escape codes which look like [0m,[1m or [32m
+	require.NotRegexp(t, `\[\d*m`, out, "Output should not contain color escape codes")
 }
