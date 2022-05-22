79aec9a2d3b4a5e38962faf16909b524b93c48ec
dkool <dennis.kool@cloudnation.nl>
2022-05-22 19:57:44 +0200

    trigger build

--- main.tf
+++ main.tf
@@ -119,3 +119,4 @@ resource "azurerm_subnet_network_security_group_association" "nsg_as" {
   subnet_id                 = azurerm_subnet.subnets[each.key].id
   network_security_group_id = azurerm_network_security_group.nsg[each.key].id
 }
+
